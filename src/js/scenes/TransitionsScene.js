ef8c20f7dd9c783f1d3cae04b4226d4d2460db3d
Roberto GM <drowlerd2000@gmail.com>
2021-10-08 02:26:23 +0900

    Tweening added to cameraEffects

--- src/js/scenes/TransitionsScene.js
+++ src/js/scenes/TransitionsScene.js
@@ -1,19 +1,19 @@
 "use strict";
-import {cameraFadeIn, cameraFadeOut, cameraShake, Transtion} from '../util/cameraEffects.js'
+import {cameraFadeIn, cameraFadeOut, cameraShake, SweepTransition} from '../util/cameraEffects.js'
 export class TransitionsScene extends Phaser.Scene{
 
     constructor() {
         super("TimerScene");
     }
     init(){
-        this.transition= new Transtion(this);
+        this.transition= new SweepTransition(this);
     }
     preload(){
         this.transition.loadTransition(this);
         this.load.image("treasure","./Resources/assets/items/treasure.png")
     }
     create(){
-        this.transition.addTransition(this);
+        this.transition.addToScene(this);
         let treasure = this.add.sprite(100,100,"treasure");
 
         // treasure.displayWidth=1000
@@ -21,10 +21,10 @@ export class TransitionsScene extends Phaser.Scene{
         console.log(this.game.config.width)
         // cameraFadeOut(this,1000,()=>cameraFadeIn(this,1000))
         console.log("Transition Scene created")
+        this.transition.playTransition(this)
     }
 
     update(){
-        this.transition.move()
     }
 }
 
